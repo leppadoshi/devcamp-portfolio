f648acf4558d2cfb7a93dff1bfc7b6cc975c1d9d
leppadoshi <3402009-gdoshi@users.noreply.gitlab.com>
2020-01-05 08:07:38 -0800

    Set up active storage for image uploading and set up AWS S3

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,13 @@
 class Portfolio < ApplicationRecord
 	has_many :technologies, dependent: :destroy
+	has_one_attached :thumb_image
+	has_one_attached :main_image
 	accepts_nested_attributes_for :technologies, 
 									reject_if: lambda { |attrs| attrs['name'].blank? }
 
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
 
 #option 1 (best, custom scope)
 	def self.angular
@@ -20,4 +23,16 @@ class Portfolio < ApplicationRecord
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
 		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
+
+	def main_image_url
+    if self.main_image.attachment
+      self.main_image.attachment.service_url
+    end
+  end
+
+  def thumb_image_url
+    if self.thumb_image.attachment
+      self.thumb_image.attachment.service_url
+    end
+  end
 end
